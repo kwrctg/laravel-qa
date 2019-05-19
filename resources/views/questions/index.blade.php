7786261b7e613be68586d9c9d47457331b3af211
Kawsar <kwrctg@users.noreply.github.com>
2019-05-19 12:43:56 +0600

    Updateing question and customiz form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -30,7 +30,12 @@
                                 </div>
                             </div>
                             <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <div class="d-flex align-items-center">
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                    <div class="ml-auto">
+                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-outline-info btn-sm">Edit</a>
+                                    </div>
+                                </div>
                                 <p class="lead">
                                     Asked by
                                     <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
