5330d14f64233e0dc958c854407584e4bc6c73c3
Kawsar <kwrctg@users.noreply.github.com>
2019-05-19 15:29:30 +0600

    Add url slug

--- app/Question.php
+++ app/Question.php
@@ -22,7 +22,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return route('questions.show', $this->id);
+        return route('questions.show', $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -41,5 +41,10 @@ class Question extends Model
         }
 
         return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
