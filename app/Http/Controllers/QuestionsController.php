6486e6d35c36df0f50738ce42a49090a13917170
Kawsar <kwrctg@users.noreply.github.com>
2019-05-19 14:52:12 +0600

    Delete question method

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -85,6 +85,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect()->route('questions.index')->with('success', 'Your question has been deleted');
     }
 }
