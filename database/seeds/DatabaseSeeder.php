3b1cd6ae1114816187d817554ef13f68cca309b3
Kawsar <kwrctg@users.noreply.github.com>
2019-05-20 15:14:48 +0600

    Generate fake answers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 3)->create()->each(function($user){
-            $user->questions()
-                 ->saveMany(
-                    factory(App\Question::class, rand(1, 5))->make()
-                 );
+        factory(App\User::class, 3)->create()->each(function($u) {
+            $u->questions()
+              ->saveMany(
+                  factory(App\Question::class, rand(1, 5))->make()
+              )
+              ->each(function ($q) {
+                $q->answers()->saveMany(factory(App\Answer::class, rand(1, 5))->make());
+              });
         });
-        factory(App\Question::class, 10)->create();
     }
 }
