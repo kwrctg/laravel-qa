39e75fd5da141cde1922711932c88271e6c22f35
Kawsar <kwrctg@users.noreply.github.com>
2019-05-21 12:28:42 +0600

    Display answers for question

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+
+// Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
+Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
